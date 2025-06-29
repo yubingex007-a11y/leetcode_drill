c8f16ca32f8990d9f30c67f7a55c47410ba1daa1
yubingex007-a11y <yubingex007@gmail.com>
2025-06-29 15:39:02 +0800

    Time: 0 ms (100%), Space: 167.3 MB (60.6%) - LeetHub

---
+++ 3732-calculate-score-after-performing-instructions/3732-calculate-score-after-performing-instructions.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long calculateScore(vector<string>& instructions, vector<int>& values) {
+        int index =0;
+        long long ans =0;
+        vector<bool> vis(values.size());
+        while(index < values.size() && !vis[index]) {
+            vis[index] = true;
+            if(instructions[index][0] == 'j') {
+                
+                index += values[index];
+                //cout<< "jump " <<index<<endl;
+                
+            } else if(instructions[index][0] == 'a') {
+                //cout<< "add " <<values[index]<<endl;
+                ans += values[index];
+                index++;
+                //cout<< "add_jump " <<index<<endl;
+                
+            }
+        }
+        return ans;
+    }
+};
